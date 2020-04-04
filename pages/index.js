3c8249db1634539f2d1bf6cecf2a031c6ee8a916
Nathaniel Cho <natejcho@gmail.com>
2020-04-04 15:03:58 -0500

    link to a dev env

--- pages/index.js
+++ pages/index.js
@@ -14,16 +14,16 @@ function Index(props) {
   // console.log(data);
   return (
     <Layout>
-      <span>Chamber: {props.recentBills[0].chamber}</span>
+      <span>Chamber: {props.recentBills.chamber}</span>
       <table>
         <tbody>
-          {props.recentBills[0].bills.map((card, index) => (
+          {props.recentBills.bills.map((card, index) => (
             <Card
               {...card}
               key={card.bill_id}
               index={index + 1}
               comments={props.billsMap[card.bill_id].comments}
-              congress={props.recentBills[0].congress}
+              congress={props.recentBills.congress}
               votes={props.billsMap[card.bill_id].votes}
             />
           ))}
@@ -42,6 +42,7 @@ export async function getServerSideProps() {
     },
   });
   const data = await res.json();
+  const recentBills = data.results[0];
   const snapshot = await db.collection("posts").get();
   const billsMap = {};
   snapshot.forEach((doc) => {
@@ -49,9 +50,23 @@ export async function getServerSideProps() {
       ...doc.data(),
     };
   });
+  recentBills.bills.forEach((b) => {
+    if (!billsMap[b.bill_id]) {
+      // TODO: i want to fire these off in a batch async
+      console.log('creating document for post: ' + b.bill_id);
+      billsMap[b.bill_id] = {
+        comments: [],
+        votes: 0,
+        leftVotes: 0,
+        moderateVotes: 0,
+        rightVotes: 0,
+      };
+      db.collection("posts").doc(b.bill_id).set(billsMap[b.bill_id], { merge: true });
+    }
+  });
   return {
     props: {
-      recentBills: data.results,
+      recentBills,
       billsMap,
     },
   };
